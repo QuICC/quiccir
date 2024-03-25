dc7ce3675af1a28f46bf04bdc0b5451891999c10
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2024-03-25 16:21:20 +0100

    pass to set fr.int

--- lib/Quiccir/Transforms/SetViewLayout.cpp
+++ lib/Quiccir/Transforms/SetViewLayout.cpp
@@ -19,24 +19,74 @@ using namespace mlir::quiccir;
 namespace {
 struct QuiccirSetViewLayout : public QuiccirSetViewLayoutBase<QuiccirSetViewLayout> {
   using QuiccirSetViewLayoutBase<QuiccirSetViewLayout>::QuiccirSetViewLayoutBase;
-  void runOnOperation() final {
-    auto module = getOperation();
-    auto *ctx = &getContext();
 
-    RewritePatternSet patterns(ctx);
-    ConversionTarget target(*ctx);
+  LogicalResult initializeOptions(StringRef options) final;
 
+  void runOnOperation() final;
 
-
-    // // in patterns for other dialects.
-    // target.addLegalOp<ModuleOp, UnrealizedConversionCastOp>();
-    // target.addLegalDialect<LLVM::LLVMDialect>();
-
-    // if (failed(applyFullConversion(module, target, std::move(patterns))))
-    //     signalPassFailure();
-  }
+  private:
+  std::string _lay0;
 };
 } // namespace
+
+LogicalResult QuiccirSetViewLayout::initializeOptions(StringRef options) {
+  if (failed(Pass::initializeOptions(options)))
+    return failure();
+  _lay0 = setViewLayout;
+  return success();
+}
+
+void QuiccirSetViewLayout::runOnOperation() {
+  auto module = getOperation();
+  auto *ctx = &getContext();
+
+  RewritePatternSet patterns(ctx);
+  ConversionTarget target(*ctx);
+
+  llvm::errs() << _lay0 << '\n';
+
+  // mapping needs to passed in as option
+  // op -> layout, str to str? op to str?
+
+  // walk the tree and process quiccir ops with missing
+  // layout
+
+  // Walk from root func
+  WalkResult result = getOperation()->walk([&](Operation* op) {
+      if (auto frIntOp = dyn_cast<FrIOp>(op)) {
+        // check if attribute is set
+        for (auto ret : op->getResults()) {
+          Type retTy = ret.getType();
+          if (auto tensor = retTy.dyn_cast<RankedTensorType>()) {
+            if (!tensor.getEncoding()) {
+              Attribute encoding = get<StringAttr>(ctx, _lay0);
+              auto shape = tensor.getShape();
+              auto eleTy = tensor.getElementType();
+              auto plusAttrTy = get<RankedTensorType>(ctx, shape, eleTy, encoding);
+              ret.setType(plusAttrTy);
+              llvm::errs() << op->getName() << " ret has no ecoding\n";
+            }
+          }
+        }
+      }
+
+      // // return deallocateBuffers(op);
+      // if (llvm::isa<QuiccirDialect>(op->getDialect())) {
+      //   llvm::errs() << op->getName() << '\n';
+      // }
+
+      // if (failed(deallocateBuffers(op)))
+      //   return WalkResult::interrupt();
+      return WalkResult::advance();
+    });
+
+  if (result.wasInterrupted())
+    signalPassFailure();
+
+
+  // if (failed(applyFullConversion(module, target, std::move(patterns))))
+  //     signalPassFailure();
+}
 
 std::unique_ptr<Pass> mlir::quiccir::createSetViewLayoutPass() {
   return std::make_unique<QuiccirSetViewLayout>();
