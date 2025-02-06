4f110a580ecd3079d5b1cad12f0dc9055a40959c
Giacomo Castiglioni <giacomo.castiglioni@erdw.ethz.ch>
2025-02-06 17:59:37 +0100

    grouping pass

--- lib/Quiccir/Transforms/TransformGrouping.cpp
+++ lib/Quiccir/Transforms/TransformGrouping.cpp
@@ -1,6 +1,6 @@
-//====- TransformGrouping.cpp - Group Transform ops ---------------===//
+//====- TransposeGrouping.cpp - Group Transpose ops ---------------===//
 //
-// This file implements a Grouping of N akin transform.
+// This file implements a Grouping of 'group' akin transpose.
 //
 //===----------------------------------------------------------------------===//
 
@@ -9,74 +9,135 @@
 
 #include "Quiccir/IR/QuiccirDialect.h"
 #include "Quiccir/IR/QuiccirOps.h"
-#include "mlir/IR/Matchers.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+#include "mlir/Dialect/Func/Transforms/FuncConversions.h"
 
 using namespace mlir;
 using namespace mlir::quiccir;
 
+namespace mlir::quiccir {
+#define GEN_PASS_DEF_QUICCIRTRANSPOSEGROUPING
+#include "Quiccir/Transforms/QuiccirPasses.h.inc"
+} // namespace mlir::quiccir
+
 namespace {
 
-bool isSameTransform(Operation *lhsOp, Operation *rhsOp) {
-  /// \todo add transform/projection interface
-  auto isTransform = [](Operation *op) {
-    return isa<FrIOp>(op) || isa<FrPOp>(op) || isa<AlIOp>(op) ||
-           isa<AlPOp>(op) || isa<JWIOp>(op) || isa<JWPOp>(op);
+bool isSameTranspose(TransposeOp lhsOp, TransposeOp rhsOp) {
+  auto checkPerm = [](const llvm::ArrayRef<int64_t> lhsPerm,
+                      const llvm::ArrayRef<int64_t> rhsPerm) {
+    if (lhsPerm.size() != rhsPerm.size()) {
+      return false;
+    }
+    for (std::size_t i = 0; i < lhsPerm.size(); i++) {
+      if (lhsPerm[i] != rhsPerm[i]) {
+        return false;
+      }
+    }
+    return true;
   };
-  auto isLhsTransform = isTransform(lhsOp);
-  auto isRhsTransform = isTransform(lhsOp);
-  if (isLhsTransform && isRhsTransform) {
-    bool isSame = (lhsOp->getName() == rhsOp->getName());
-    bool isSameKind = (lhsOp->getAttr("kind") == rhsOp->getAttr("kind"));
-    return isSame && isSameKind;
-  }
-  return false;
+  bool isSamePemutation = checkPerm(lhsOp.getPermutation(), rhsOp.getPermutation());
+  auto checkType = [](RankedTensorType lhsType, RankedTensorType rhsType) {
+    return lhsType == rhsType;
+  };
+  bool isSameInputType = checkType(lhsOp.getInput()[0].getType().cast<RankedTensorType>(),
+                                   rhsOp.getInput()[0].getType().cast<RankedTensorType>());
+  return isSamePemutation && isSameInputType;
 }
 
-//===----------------------------------------------------------------------===//
-// TransposeGrouping
-//===----------------------------------------------------------------------===//
-struct TransposeGrouping : public OpRewritePattern<TransposeOp> {
-  TransposeGrouping(MLIRContext *ctx)
-      : OpRewritePattern<TransposeOp>(ctx, /*benefit=*/1){};
-
-  LogicalResult matchAndRewrite(TransposeOp op,
-                                PatternRewriter &rewriter) const final {
-
-      return success();
-    }
-
-    return failure();
-  }
-};
 
 } // namespace
 
 //===----------------------------------------------------------------------===//
-// QuiccirTransformGroupingPass
+// QuiccirTransposeGroupingPass
 //===----------------------------------------------------------------------===//
 
 /// This is a rewrite pass
 namespace {
-struct QuiccirTransformGroupingPass
-    : public QuiccirTransformGroupingBase<QuiccirTransformGroupingPass> {
+struct QuiccirTransposeGroupingPass
+    : public quiccir::impl::QuiccirTransposeGroupingBase<QuiccirTransposeGroupingPass> {
+  using QuiccirTransposeGroupingBase<QuiccirTransposeGroupingPass>::QuiccirTransposeGroupingBase;
   void runOnOperation() final;
 };
+
 } // namespace
 
-void QuiccirTransformGroupingPass::runOnOperation() {
+void QuiccirTransposeGroupingPass::runOnOperation() {
+  func::FuncOp funcOp = getOperation();
+  // Walk from root func
+  SmallVector<Operation *, 4> transposeOps;
+  bool needToGroup = false;
+  WalkResult result = funcOp.walk([&](Operation *op) {
+    // Get the first transpose op that is not grouped
+    if (auto transposeOp = dyn_cast<TransposeOp>(op)) {
+      // Check if the transpose op has more than one result
+      if (transposeOp->getNumResults() > 1) {
+        // skip
+        return WalkResult::advance();
+      }
+      else {
 
-  RewritePatternSet patterns(&getContext());
-  patterns.add<TransposeGrouping>(&getContext());
+        // Is this the first transpose with a single result?
+        if (transposeOps.empty()) {
+          // Then store it
 
-  FrozenRewritePatternSet patternSet(std::move(patterns));
-  if (failed(applyPatternsAndFoldGreedily(getOperation(), patternSet)))
+          transposeOps.push_back(transposeOp);
+          return WalkResult::advance();
+        }
+        else {
+
+          // Check if the transpose op is the same as the collected ones
+          if (isSameTranspose(dyn_cast<TransposeOp>(transposeOps.front()), transposeOp)) {
+
+            // Collect
+            transposeOps.push_back(transposeOp);
+            // Have we collected enough?
+            if (static_cast<int>(transposeOps.size()) == group) {
+              // Then rewrite
+              needToGroup = true;
+              return WalkResult::interrupt();
+            }
+          }
+        }
+      }
+    }
+    return WalkResult::advance();
+  });
+
+  if (result.wasInterrupted() && !needToGroup) {
     signalPassFailure();
+  }
+  else {
+    // Otherwise group the transposes
+    // Collect inputs and return types
+    SmallVector<Value, 4> inputs;
+    SmallVector<Type, 4> resultTypes;
+    // SmallVector<NamedAttribute, 4> attributes;
+    for (auto transposeOp : transposeOps) {
+      inputs.push_back(cast<TransposeOp>(transposeOp).getInput()[0]);
+      resultTypes.push_back(cast<TransposeOp>(transposeOp).getResult(0).getType());
+    }
+
+    // Set builder and insertion point
+    OpBuilder builder(funcOp);
+    builder.setInsertionPoint(transposeOps.front());
+
+    // Create a new transpose op
+    auto newTranspose = builder.create<TransposeOp>(transposeOps.front()->getLoc(),
+      resultTypes, inputs, cast<TransposeOp>(transposeOps.front()).getPermutation(), ::mlir::IntegerAttr{});
+
+    // Replace the old transpose uses with the new transpose values
+    for (std::size_t i = 0; i < transposeOps.size(); i++) {
+      transposeOps[i]->getResult(0).replaceAllUsesWith(newTranspose.getResult(i));
+      transposeOps[i]->erase();
+    }
+  }
 }
 
 /// Create a pass for lowering operations to library calls
-std::unique_ptr<Pass> mlir::quiccir::createTransformGroupingPass() {
-  return std::make_unique<QuiccirTransformGroupingPass>();
+std::unique_ptr<Pass> mlir::quiccir::createTransposeGroupingPass() {
+  return std::make_unique<QuiccirTransposeGroupingPass>();
+}
+
+std::unique_ptr<Pass> mlir::quiccir::createTransposeGroupingPass(const QuiccirTransposeGroupingOptions &options) {
+  return std::make_unique<QuiccirTransposeGroupingPass>(options);
 }
--- test/Quiccir/transpose-grouping.mlir
+++ test/Quiccir/transpose-grouping.mlir
@@ -1,10 +1,10 @@
-// RUN: quiccir-opt %s --quiccir-transform-group | FileCheck %s
+// RUN: quiccir-opt %s --quiccir-transpose-group | FileCheck %s
 
 module {
-  // CHECK: func.func @entryGroupAll(%[[A0:.*]]: tensor<?x?x?xcomplex<f64>>, %[[A1:.*]]: tensor<?x?x?xcomplex<f64>>) -> tensor<?x?x?xcomplex<f64>> {
+  // CHECK: func.func @entryGroupAll(%[[A0:.*]]: tensor<?x?x?xcomplex<f64>>, %[[A1:.*]]: tensor<?x?x?xcomplex<f64>>) -> (tensor<?x?x?xcomplex<f64>>, tensor<?x?x?xcomplex<f64>>) {
   // CHECK: %[[TRA:.*]]:2 = quiccir.transpose %[[A0]], %[[A1]] permutation = [1, 2, 0] : tensor<?x?x?xcomplex<f64>>, tensor<?x?x?xcomplex<f64>> -> tensor<?x?x?xcomplex<f64>>, tensor<?x?x?xcomplex<f64>>
   // CHECK: return %[[TRA]]#0, %[[TRA]]#1 : tensor<?x?x?xcomplex<f64>>, tensor<?x?x?xcomplex<f64>>
-  func.func @entryGroupAll(%alphys0: tensor<?x?x?xcomplex<f64>>, %alphys1: tensor<?x?x?xcomplex<f64>>) -> tensor<?x?x?xcomplex<f64>> {
+  func.func @entryGroupAll(%alphys0: tensor<?x?x?xcomplex<f64>>, %alphys1: tensor<?x?x?xcomplex<f64>>) -> (tensor<?x?x?xcomplex<f64>>, tensor<?x?x?xcomplex<f64>>) {
   %ftmod0 = quiccir.transpose %alphys0 permutation = [1, 2, 0] : tensor<?x?x?xcomplex<f64>> -> tensor<?x?x?xcomplex<f64>>
   %ftmod1 = quiccir.transpose %alphys1 permutation = [1, 2, 0] : tensor<?x?x?xcomplex<f64>> -> tensor<?x?x?xcomplex<f64>>
   return %ftmod0, %ftmod1  : tensor<?x?x?xcomplex<f64>>, tensor<?x?x?xcomplex<f64>>
